5103c7e53e91e735aa63ffe82034cf58bed6cfc9
latrelheron <latrel.heron@outlook.com>
2026-08-12 23:44:36 +1000

    Add, function activity screen, with activity screen name so idenitiable once clicking button to access activity screen

--- app/src/main/java/com/example/assessment3/ui/activity/ActivityScreen.kt
+++ app/src/main/java/com/example/assessment3/ui/activity/ActivityScreen.kt
@@ -1,2 +1,9 @@
 package com.example.assessment3.ui.activity
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ActivityScreen() {
+    Text("Activity Screen")
+}
