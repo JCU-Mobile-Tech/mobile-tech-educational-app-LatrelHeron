dbf316f8160077eede71c3bd11a0527c7d31529f
latrelheron <latrel.heron@outlook.com>
2026-08-13 00:06:14 +1000

    Add, Routes for the core screens with links to all screens from the landing screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
--- app/src/main/java/com/example/assessment3/ui/navigation/AppNavGraph.kt
+++ app/src/main/java/com/example/assessment3/ui/navigation/AppNavGraph.kt
@@ -1,2 +1,30 @@
 package com.example.assessment3.ui.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.assessment3.ui.activity.ActivityScreen
+import com.example.assessment3.ui.landing.LandingScreen
+import com.example.assessment3.ui.statistics.StatisticsScreen
+
+@Composable
+fun AppNavGraph(
+    navController: NavHostController
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Routes.LANDING
+    ) {
+        composable(Routes.LANDING) {
+            LandingScreen(
+                onStartActivity = { navController.navigate(Routes.ACTIVITY) },
+                onOpenStatistics = { navController.navigate(Routes.STATISTICS) },
+                onOpenSettings = { navController.navigate(Routes.SETTINGS)}
+            )
+        }
+        composable(Routes.ACTIVITY) { ActivityScreen() }
+        composable(Routes.STATISTICS) { StatisticsScreen() }
+        composable(Routes.SETTINGS) { StatisticsScreen() }
+    }
+}
