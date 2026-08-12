4dde63ad3002bac2e4d1d257a0f02a9ab6d48039
latrelheron <latrel.heron@outlook.com>
2026-08-12 23:49:58 +1000

    Add, function Settings and Statisitics screen, with text so screen identifiable in clicking the button

--- app/src/main/java/com/example/assessment3/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/assessment3/ui/settings/SettingsScreen.kt
@@ -1,2 +1,9 @@
 package com.example.assessment3.ui.settings
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SettingsScreen() {
+    Text("Settings Screen")
+}
--- app/src/main/java/com/example/assessment3/ui/statistics/StatisiticsScreen.kt
+++ app/src/main/java/com/example/assessment3/ui/statistics/StatisiticsScreen.kt
@@ -1,2 +1,9 @@
 package com.example.assessment3.ui.statistics
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun StatisticsScreen() {
+    Text("Statistics Screen")
+}
