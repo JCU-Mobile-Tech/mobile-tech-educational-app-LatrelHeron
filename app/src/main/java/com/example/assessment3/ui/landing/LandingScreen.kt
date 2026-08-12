3b8e96336777646cccbe330e28dc94a3ac7d7a88
latrelheron <latrel.heron@outlook.com>
2026-08-12 23:40:52 +1000

    Add, function Landing screen into Landing screen kt file with core screen buttons

--- app/src/main/java/com/example/assessment3/ui/landing/LandingScreen.kt
+++ app/src/main/java/com/example/assessment3/ui/landing/LandingScreen.kt
@@ -1,2 +1,27 @@
 package com.example.assessment3.ui.landing
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LandingScreen(
+    onStartActivity: () -> Unit,
+    onOpenStatistics: () -> Unit,
+    onOpenSettings: () -> Unit
+) {
+    Column {
+        Button(onClick = onStartActivity) {
+            Text("Start Activity")
+        }
+
+        Button(onClick = onOpenStatistics) {
+            Text("Statistics")
+        }
+
+        Button(onClick = onOpenSettings) {
+            Text("Settings")
+        }
+    }
+}
