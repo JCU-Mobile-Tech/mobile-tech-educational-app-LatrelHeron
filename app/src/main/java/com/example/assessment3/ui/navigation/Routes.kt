cc6fc6ce247ddb55550cdd901dde469db526cd74
latrelheron <latrel.heron@outlook.com>
2026-08-12 23:32:44 +1000

    Add, values to object Routes

--- app/src/main/java/com/example/assessment3/ui/navigation/Routes.kt
+++ app/src/main/java/com/example/assessment3/ui/navigation/Routes.kt
@@ -1,2 +1,8 @@
 package com.example.assessment3.ui.navigation
 
+object Routes {
+    const val LANDING = "landing"
+    const val ACTIVITY = "activity"
+    const val STATISTICS = "statistics"
+    const val SETTINGS = "settings"
+}
