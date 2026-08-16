a58be69ad2142ffe9e4956be05489ea16872924c
latrelheron <latrel.heron@outlook.com>
2026-08-16 11:49:07 +1000

    Update to include settings and API repos

--- app/src/main/java/com/example/assessment3/ui/activity/ActivityViewModelFactory.kt
+++ app/src/main/java/com/example/assessment3/ui/activity/ActivityViewModelFactory.kt
@@ -3,9 +3,12 @@ package com.example.assessment3.ui.activity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.assessment3.data.repository.QuizRepository
+import com.example.assessment3.data.repository.MathFactRepository
 
 class ActivityViewModelFactory(
-    private val quizRepository: QuizRepository
+    private val quizRepository: QuizRepository,
+    private val settingsRepository: SettingsRepository,
+    private val mathFactRepository: MathFactRepository
 ) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(
@@ -15,7 +18,9 @@ class ActivityViewModelFactory(
         if (modelClass.isAssignableFrom(ActivityViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
             return ActivityViewModel(
-                quizRepository = quizRepository
+                quizRepository = quizRepository,
+                settingsRepository = settingsRepository,
+                mathFactRepository = mathFactRepository
             ) as T
         }
 
