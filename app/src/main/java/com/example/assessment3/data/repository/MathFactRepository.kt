2ebe0ab9cb800a5add24122e70a49753dc5fc850
latrelheron <latrel.heron@outlook.com>
2026-08-16 11:32:10 +1000

    Add math fact repository

---
+++ app/src/main/java/com/example/assessment3/data/repository/MathFactRepository.kt
@@ -0,0 +1,12 @@
+package com.example.assessment3.data.repository
+
+import com.example.assessment3.data.remote.MathFactApiService
+
+class MathFactRepository(
+    private val apiService: MathFactApiService
+) {
+
+    suspend fun getMathFact(number: Int): String {
+        return apiService.getMathFact(number)
+    }
+}
